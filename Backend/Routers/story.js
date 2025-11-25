e5ff01055226e02da08ea6bc255af47cedd05ef1
palle sai sowjanya <144721965+pallesowjanya@users.noreply.github.com>
2025-11-26 00:07:53 +0530

    Update story.js

--- Backend/Routers/story.js
+++ Backend/Routers/story.js
@@ -1,26 +1,71 @@
-const express = require("express")
+const express = require("express");
 const imageupload = require("../Helpers/Libraries/imageUpload");
 
 const { getAccessToRoute } = require("../Middlewares/Authorization/auth");
-const {addStory,getAllStories,detailStory,likeStory, editStory, deleteStory, editStoryPage } = require("../Controllers/story")
-const { checkStoryExist, checkUserAndStoryExist } = require("../Middlewares/database/databaseErrorhandler");
+const { 
+  addStory, 
+  getAllStories, 
+  detailStory, 
+  likeStory, 
+  editStory, 
+  deleteStory, 
+  editStoryPage 
+} = require("../Controllers/story");
 
-const router = express.Router() ;
+const { 
+  checkStoryExist, 
+  checkUserAndStoryExist 
+} = require("../Middlewares/database/databaseErrorhandler");
 
-router.post("/addstory" ,[getAccessToRoute, imageupload.single("image")],addStory)
-
-
-router.post("/:slug", checkStoryExist, detailStory)
-
-router.post("/:slug/like",[getAccessToRoute,checkStoryExist] ,likeStory)
-
-router.get("/editStory/:slug",[getAccessToRoute,checkStoryExist,checkUserAndStoryExist] , editStoryPage)
-
-router.put("/:slug/edit",[getAccessToRoute,checkStoryExist,checkUserAndStoryExist, imageupload.single("image")] ,editStory)
-
-router.delete("/:slug/delete",[getAccessToRoute,checkStoryExist,checkUserAndStoryExist] ,deleteStory)
-
-router.get("/getAllStories",getAllStories)
+const router = express.Router();
 
 
-module.exports = router
+// CUSTOM MULTER ERROR HANDLER WRAPPER
+const uploadStoryImage = (req, res, next) => {
+  imageupload.single("image")(req, res, (err) => {
+    if (err) {
+      // send error to global handler
+      return next(err);
+    }
+    next();
+  });
+};
+
+
+// -------- ROUTES ---------
+
+router.post(
+  "/addstory",
+  [getAccessToRoute, uploadStoryImage],
+  addStory
+);
+
+
+router.post("/:slug", checkStoryExist, detailStory);
+
+router.post("/:slug/like",
+  [getAccessToRoute, checkStoryExist],
+  likeStory
+);
+
+router.get(
+  "/editStory/:slug",
+  [getAccessToRoute, checkStoryExist, checkUserAndStoryExist],
+  editStoryPage
+);
+
+router.put(
+  "/:slug/edit",
+  [getAccessToRoute, checkStoryExist, checkUserAndStoryExist, uploadStoryImage],
+  editStory
+);
+
+router.delete(
+  "/:slug/delete",
+  [getAccessToRoute, checkStoryExist, checkUserAndStoryExist],
+  deleteStory
+);
+
+router.get("/getAllStories", getAllStories);
+
+module.exports = router;
